9a8533f45da2cb6b72f447997b12c2450cc1cae2
Andrew Southern <whoamihealay@gmail.com>
2022-02-17 10:24:51 -0500

    customized base app component with css and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
-import "./App.css";
+
+// eslint-disable-next-line global-require
+const br = require("./images/bottom-right.svg") as string;
+// eslint-disable-next-line global-require
+const tl = require("./images/top-left.svg") as string;
+
+// import br from "./images/bottom-right.svg";
+// import tl from "./images/top-left.svg";
 
 function App() {
-  return <div className="text-red-600">Hi !</div>;
+  return (
+    <>
+      <img src={tl} alt="" className="absolute" />
+      <img src={br} alt="" className="absolute bottom-0 right-0" />
+      <div className="flex min-h-screen items-center justify-center bg-slate-800">
+        <h1 className="text-9xl text-white">Hi !</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
